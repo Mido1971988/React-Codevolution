f389827e5b72168daa3e62ae4ce1ff38a6f0ec0c
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-01-09 19:39:17 +0300

    daily

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,11 @@
+// Functional Component
+
+import React from "react"
+
+// function Greet(){
+//     return <h1>Hello Soliman</h1>
+// }
+
+const Greet = () => <h1>Hello Soliman</h1>
+
+export default Greet
