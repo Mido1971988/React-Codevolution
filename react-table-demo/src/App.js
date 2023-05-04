4d11fe68d4a2548d411ca237d815a8f0339075c0
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-05-04 20:52:17 +0300

    Daily

--- react-table-demo/src/App.js
+++ react-table-demo/src/App.js
@@ -4,6 +4,10 @@ import { BasicTable } from './components/BasicTable';
 import { SortingTable } from './components/SortingTable';
 import { FilteringTable } from './components/FilteringTable';
 import { PaginationTable } from './components/PaginationTable';
+import { RowSelection } from './components/RowSelection';
+import { ColumnOrder } from './components/ColumnOrder';
+import { ColumnHiding } from './components/ColumnHiding';
+import { StickyTable } from './components/StickyTable';
 
 function App() {
   return (
@@ -11,7 +15,11 @@ function App() {
       {/* <BasicTable/> */}
       {/* <SortingTable/> */}
       {/* <FilteringTable/> */}
-      <PaginationTable/>
+      {/* <PaginationTable/> */}
+      {/* <RowSelection/> */}
+      {/* <ColumnOrder/> */}
+      {/* <ColumnHiding/> */}
+      <StickyTable/>
     </>
   );
 }
