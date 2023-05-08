d685788bcd57e7fd974b39bd5b605ac0c8eb39c7
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-05-09 00:51:21 +0300

    Daily

--- react-storybook-v6/.storybook/preview.js
+++ react-storybook-v6/.storybook/preview.js
@@ -1,4 +1,6 @@
 /** @type { import('@storybook/react').Preview } */
+import Center from '../src/components/Center/Center'
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,12 +9,13 @@ const preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
-    },
+    }, 
     options: {
       storySort: (a, b) =>
         a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true }),
     },
   },
+  decorators: [Story => (<Center><Story/></Center>)]
 };
 
 export default preview;
