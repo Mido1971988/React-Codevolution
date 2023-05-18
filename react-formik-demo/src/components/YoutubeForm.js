c05b8334739d8af149db06c366e7fdc2579c89b7
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-05-18 19:14:04 +0300

    Daily

--- react-formik-demo/src/components/YoutubeForm.js
+++ react-formik-demo/src/components/YoutubeForm.js
@@ -2,11 +2,18 @@ import React from 'react'
 // import { useFormik } from 'formik'
 import { Formik , Form , Field , ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+import TextError from './TextError'
 
 const initialValues = {
     name: 'Vishwas',
     email: '',
-    channel: ''
+    channel: '',
+    comments: '',
+    address: '',
+    social: {
+        facebook: '',
+        twitter: ''
+    }
 }
 
 const onSubmit = values => {
@@ -19,23 +26,26 @@ const validationSchema = Yup.object({
     email: Yup.string()
         .email('Invalid email format')
         .required('Required'),
-    channel: Yup.string().required('Required')
+    channel: Yup.string().required('Required'),
+    address : Yup.string().required('Required')
 })
 
 function YoutubeForm() {
     return (
         <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
             <Form>
-                <div className='form-control'>  
+                <div className='form-control'>
                     <label htmlFor='name'>Name</label>
-                    <Field type='text' name='name' id='name'/>
-                    <ErrorMessage name='name'/>
+                    <Field type='text' id='name' name='name' />
+                    <ErrorMessage name='name' component={TextError} />
                 </div>
 
-                <div className='form-control'> 
-                    <label htmlFor='name'>Email</label>
-                    <Field type='email' name='email' id='email' />
-                    <ErrorMessage  name='email'/>
+                <div className='form-control'>
+                    <label htmlFor='email'>Email</label>
+                    <Field type='email' id='email' name='email' />
+                    <ErrorMessage name='email'>
+                        {error => <div className='error'>{error}</div>}
+                    </ErrorMessage>
                 </div>
 
                 <div className='form-control'> 
@@ -44,7 +54,44 @@ function YoutubeForm() {
                     <ErrorMessage name='channel'/>
                 </div>
 
-                <button type='submit'>Submit</button>
+                <div className='form-control'>
+                <label htmlFor='comments'>Comments</label>
+                <Field
+                    as='textarea'
+                    id='comments'
+                    name='comments'
+                />
+                <ErrorMessage name='comments' />
+                </div>
+
+                <div className='form-control'>
+                <label htmlFor='address'>Address</label>
+                <Field name='address'>
+                    {(props) => {
+                        const { field, form, meta } = props
+                    return (
+                        <div>
+                        <input type='text' {...field} />
+                        {meta.touched && meta.error ? (
+                            <div>{meta.error}</div>
+                        ) : null}
+                        </div>
+                    )
+                    }}
+                </Field>
+            </div>
+            
+            <div className='form-control'>
+                <label htmlFor='facebook'>Facebook profile</label>
+                <Field type='text' id='facebook' name='social.facebook' />
+            </div>
+
+            <div className='form-control'>
+                <label htmlFor='twitter'>Twitter profile</label>
+                <Field type='text' id='twitter' name='social.twitter' />
+            </div>
+
+            <button type='submit'>Submit</button>
             </Form>
         </Formik>
     )
