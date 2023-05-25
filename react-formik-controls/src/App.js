1b78bd8d1a48b8f77240ef6a4a822243445d9c0d
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-05-25 18:39:26 +0300

    Daily

--- react-formik-controls/src/App.js
+++ react-formik-controls/src/App.js
@@ -2,14 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import FormikContainer from './components/FormikContainer'
 import LoginForm from './components/LoginForm';
+import RegistrationForm from './components/RegistrationForm';
+import EnrollmentForm from './components/EnrollmentForm';
+import { ThemeProvider, theme } from '@chakra-ui/react'
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <FormikContainer />
+//       <LoginForm/>
+//       <RegistrationForm />
+//       <EnrollmentForm />
+//     </div>
+//   );
+// }
+
+// Chakra 
+function App () {
   return (
-    <div className="App">
-      {/* <FormikContainer /> */}
-      <LoginForm/>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <LoginForm />
+      </div>
+    </ThemeProvider>
+  )
 }
 
 export default App;
