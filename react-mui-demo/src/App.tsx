e9ff1bfe73c7ebf5b5e405ff03795d383e2f73f0
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-06-05 19:29:23 +0300

    Daily

--- react-mui-demo/src/App.tsx
+++ react-mui-demo/src/App.tsx
@@ -3,14 +3,26 @@ import { MuiTypography } from './components/MuiTypography'
 import { MuiButton } from './components/MuiButton'
 import { MuiTextField } from './components/MuiTextField';
 import { MuiSelect } from './components/MuiSelect';
+import { MuiRadioButton } from './components/MuiRadioButton';
+import { MuiCheckbox } from './components/MuiCheckBox';
+import { MuiSwitch } from './components/MuiSwitch';
+import { MuiRating } from './components/MuiRating';
+import { MuiAutocomplete } from './components/MuiAutoComplete';
+import { MuiLayout } from './components/MuiLayout';
 
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
-      <MuiTextField/>
-      <MuiSelect/>
+      {/* <MuiTextField/> */}
+      {/* <MuiSelect/> */}
+      {/* <MuiRadioButton/> */}
+      {/* <MuiCheckbox/> */}
+      {/* <MuiSwitch/> */}
+      <MuiRating/>
+      <MuiAutocomplete/>
+      <MuiLayout/>
     </div>
   );
 }
