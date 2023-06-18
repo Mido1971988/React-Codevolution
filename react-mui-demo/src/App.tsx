fcc7ccd158bfb097dbf44ed796b0a1cd251f43b7
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-06-18 18:59:57 +0300

    Daily

--- react-mui-demo/src/App.tsx
+++ react-mui-demo/src/App.tsx
@@ -35,6 +35,9 @@ import { MuiLoadingButton } from './components/MuiLoadingButton';
 import { MuiSkeleton } from './components/MuiSkeleton';
 import { MuiDateTimePicker } from './components/MuiDateTimePicker';
 import { MuiDateRangePicker } from './components/MuiDateRangePicker';
+import { MuiTabs } from './components/MuiTabs';
+import { MuiTimeline } from './components/MuiTimeline';
+import { MuiMasonry } from './components/MuiMasonry';
 
 function App() {
   return (
@@ -71,7 +74,10 @@ function App() {
         {/* <MuiSkeleton/> */}
         {/* <MuiLoadingButton/> */}
         {/* <MuiDateTimePicker/> */}
-        <MuiDateRangePicker/>
+        {/* <MuiDateRangePicker/> */}
+        {/* <MuiTabs/> */}
+        {/* <MuiTimeline/> */}
+        <MuiMasonry/>
       </div>
     </LocalizationProvider>
   );
