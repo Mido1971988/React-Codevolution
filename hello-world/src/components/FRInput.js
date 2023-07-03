ee44aeb83ec47252baae5ad5dc61d325a7e15ec3
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-07-03 19:43:56 +0300

    fromVSCode

--- hello-world/src/components/FRInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FRInput = React.forwardRef(function FRInput(props,ref) {
-    return (
-        <input type="text" ref={ref}></input>
-    )
-})
-
-export default FRInput
---
+++ react-testing/src/components/greet/greet.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { Greet } from './greet';
+
+
+test('renders learn react link', () => {
+    render(<Greet/>);
+    // const textElement = screen.getByText(/hello/i);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+});
