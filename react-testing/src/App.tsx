f509518b45e728dab8adde8cb86fcdaa0c7c3a7c
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-07-12 20:17:37 +0300

    daily

--- react-testing/src/App.tsx
+++ react-testing/src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import { Application } from './components/application/application';
+import './App.css'
+// import { Application } from './components/application/application'
+// import { CounterTwo } from './components/counter-two/CounterTwo'
+// import { Counter } from './components/counter/Counter'
+import { MuiMode } from './components/mui/MuiMode'
+// import { Skills } from './components/skills/Skills'
+// import { Users } from './components/users/Users'
+import { AppProviders } from './providers/AppProviders'
 
 function App() {
   return (
-    <div className="App">
-      <Application/>
-    </div>
-  );
+    <AppProviders>
+      <div className="App">
+        {/* <Application /> */}
+        {/* <Skills skills={['HTML', 'CSS']} /> */}
+        {/* <Counter /> */}
+        {/* <CounterTwo count={1} /> */}
+        {/* <Users /> */}
+        <MuiMode />
+      </div>
+    </AppProviders>
+  )
 }
 
-export default App;
+export default App
