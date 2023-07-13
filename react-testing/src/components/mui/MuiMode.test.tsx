cf6324e9304efbf03469ca6e10690ca552f824fc
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-07-13 19:22:28 +0300

    daily

--- react-testing/src/components/mui/MuiMode.test.tsx
+++ react-testing/src/components/mui/MuiMode.test.tsx
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react'
+// import { render, screen } from '@testing-library/react'
+import { render, screen } from '../../test-utils'
+// import { AppProviders } from '../../providers/AppProviders'
 import { MuiMode } from './MuiMode'
 
 describe('MuiMode', () => {
     test('renders text correctly', () => {
+        // render(<MuiMode />, {
+        //     wrapper : AppProviders 
+        // })
         render(<MuiMode />)
         const headingElement = screen.getByRole('heading')
         expect(headingElement).toHaveTextContent('dark mode')
