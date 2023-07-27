6301b758fc9f2898b480cef2be7ddc7e6a2c3158
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-07-27 19:21:23 +0300

    daily

--- NEXTJS/next-routing/app/page.tsx
+++ NEXTJS/next-routing/app/page.tsx
@@ -1,11 +1,21 @@
+"use client"
+
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
+  const router = useRouter()
+  const btnHandler = ()=>{
+    router.push("./product")
+  }
   return (
     <>
       <h1>Home Page</h1>
       <Link href="/blog">Blog</Link>
+      <br/>
       <Link href="/product">Products</Link>
+      <br/>
+      <button onClick={btnHandler}>Place Order</button>
     </>
   ) 
 }
